1d4999cd0d6fb1868bc831034849fadbeb7a0809
AlexLeonov2020 <alex.leon2010@icloud.com>
2025-08-19 01:21:26 +0300

    next

--- lib/api.ts
+++ lib/api.ts
@@ -1,69 +1,48 @@
-import axios from "axios";
-import type { Note} from "@/types/note";
+import { Note } from "@/types/note";
 
-axios.defaults.baseURL = "https://notehub-public.goit.study/api";
-const myKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
-if (myKey) {
-  axios.defaults.headers.common['Authorization'] = `Bearer ${myKey}`;
-} else {
-  console.warn('NEXT_PUBLIC_NOTEHUB_TOKEN is not defined.');
-}
-
-export interface FetchNotesParams {
-    page?: number;
-    perPage?: number;
-    search?: string;
-    tag?: string;
+interface FetchNotesResponseRaw {
+  notes: Note[];
+  total_pages: number;
 }
 
 export interface FetchNotesResponse {
-    page: number;
-    data: Note[];
-    total_pages: number;
-    perPage: number;
-}
-
-interface RawFetchNotesResponse {
   notes: Note[];
   totalPages: number;
 }
 
-export const fetchNotes = async ({page = 1, perPage = 12, search = '', tag}: FetchNotesParams): Promise<FetchNotesResponse> => {
-    const response = await axios.get<RawFetchNotesResponse>('/notes', {
-        params: {
-            page,
-            perPage,
-            ...(search !== '' && { search }),
-            ...(tag && tag !== "All" ? {tag} : {}),
-        },
-    });
-    
-    const raw = response.data;
-    return {
-    page,
-    perPage,
-    data: raw.notes,
-    total_pages: raw.totalPages,
+export async function getNotes(page = 1, limit = 6): Promise<FetchNotesResponse> {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/AlexLeonov2020/api-data/main/notes.json`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch notes");
+  }
+
+  const data: FetchNotesResponseRaw = await res.json();
+
+  return {
+    notes: data.notes,
+    totalPages: data.total_pages,
   };
-};
+}
 
-export const fetchNoteById = async (id: string): Promise<Note> => {
-    const response = await axios.get<Note>(`/notes/${id}`);
-    return response.data;
-};
+export async function getNoteById(id: string): Promise<Note> {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/AlexLeonov2020/api-data/main/notes.json`
+  );
 
-export const createNote = async (note: {
-    title: string;
-    content: string;
-    tag: string;
-}): Promise<Note> => {
-    const response = await axios.post<Note>('/notes', note);
+  if (!res.ok) {
+    throw new Error("Failed to fetch note");
+  }
 
-    return response.data;
-};
+  const data: FetchNotesResponseRaw = await res.json();
 
-export const deleteNote = async (id: string): Promise<Note> => {
-    const response = await axios.delete<Note>(`/notes/${id}`);
-    return response.data;
-};
+  const note = data.notes.find((n) => n.id === id);
+
+  if (!note) {
+    throw new Error("Note not found");
+  }
+
+  return note;
+}
